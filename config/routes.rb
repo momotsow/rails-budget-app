ff4187127a8af57daa5e8081cc77c29e41be1985
momotsow <kgomotsomkansi@gmail.com>
2023-03-21 21:23:44 +0200

    Add models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :expenses_groups
+  resources :groups do
+    resources :expenses
+  end
 
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "groups#index"
 end
