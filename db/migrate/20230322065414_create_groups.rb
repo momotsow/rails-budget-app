3c01703641581147da006b24e26a0724f8686fdb
momotsow <kgomotsomkansi@gmail.com>
2023-03-22 09:05:12 +0200

    Update migrations

--- db/migrate/20230322065414_create_groups.rb
+++ db/migrate/20230322065414_create_groups.rb
@@ -1,6 +1,9 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
+      t.string :name
+      t.string :icon
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_172506) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_065541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "expense_groups", force: :cascade do |t|
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
-  create_table "expenses", force: :cascade do |t|
+  create_table "expenses_groups", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "expense_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["expense_id"], name: "index_expenses_groups_on_expense_id"
+    t.index ["group_id"], name: "index_expenses_groups_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expenses", "users"
+  add_foreign_key "expenses_groups", "expenses"
+  add_foreign_key "expenses_groups", "groups"
+  add_foreign_key "groups", "users"
 end
