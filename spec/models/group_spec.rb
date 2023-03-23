1641370888a6984fea0b1b0799f8b1f1354254e5
momotsow <kgomotsomkansi@gmail.com>
2023-03-23 08:54:38 +0200

    Add tests

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+  before :each do
+    @user = User.create(name: 'Roy Rob', email: 'rob@gmail.com', password: 123456, password_confirmation: 123456)
+  end
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      group = Group.new(name: 'Food',
+                        icon: 'https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg', user_id: @user.id)
+      expect(group).to be_valid
+    end
+
+    it 'is not valid without user id' do
+      group = Group.new(name: 'Food',
+                        icon: 'https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg', user_id: nil)
+      expect(group).to_not be_valid
+    end
+  end
+end
